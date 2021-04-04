88ad5d6fd67ca8765ac1875c8f9f9edd296f300d
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-04 19:37:18 +0200

    style: :art: Added a comments that it will use in the future

---
+++ repository/repo.py
@@ -0,0 +1,13 @@
+# from domain.Inventory import Inventory
+# from domain.normal_item import NormalItem
+# from domain.aged_brie import AgedBrie
+# from domain.backstagePasses import BackstagePasses
+# from domain.Sulfuras import Sulfuras
+# from domain.Conjured import Conjured
+
+# class Factory():
+    
+#     @staticmethod
+#     def initRepo():
+        
+        
