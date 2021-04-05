a84e349bc7422590932a6329e02a6f646ffab16b
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-05 17:48:46 +0200

    feat: :sparkles: Added a CSV file which contains Data of basic Items and with repo.py that we have Factory Class which has a method that return a list with dictionaries that are the items

--- repository/repo.py
+++ repository/repo.py
@@ -1,3 +1,4 @@
+# Classes that we use for create Object of Items
 # from domain.Inventory import Inventory
 # from domain.normal_item import NormalItem
 # from domain.aged_brie import AgedBrie
@@ -5,9 +6,59 @@
 # from domain.Sulfuras import Sulfuras
 # from domain.Conjured import Conjured
 
-# class Factory():
+# CSV
+import csv
+import os
+
+class Factory():
     
-#     @staticmethod
-#     def initRepo():
+        
+    # @staticmethod
+    # def loadInventario():
+        
+    @staticmethod
+    def initRepo():
+        
+        # tienda = Inventory()
+        
+        # filename = "C:/Users/Mateo/Documents/APUNTES_DUAL_FP_2020/PROJECTS/Flask_APIRestFul_Ollivanders/API-REST-Ollivanders/repository/data_items.csv"
+        
+        # file = open(filename, 'r')
+        
+        # reader = csv.reader(file)
+        
+        file_dir = os.path.dirname(__file__)
+        
+        filename = "data_items.csv"
+        
+        absolute_file_path = os.path.join(file_dir, filename)
+        
+        with open(absolute_file_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            
+            inventory = []
+            
+            for row in csv_reader:
+                
+                inventory.append({'name': row[0], 'sell_in': int(row[1]), 'quality': int(row[2]) })
+                
+        return inventory
+    
+class SingletonOllivanders():
+    
+    instanceOllivanders = None
+    
+    @staticmethod
+    def createStore():
+        
+        if not SingletonOllivanders.instanceOllivanders:
+            
+            SingletonOllivanders.instanceOllivanders = Factory.initRepo()
+            
+            return SingletonOllivanders.instanceOllivanders
         
         
+
+if __name__ == "__main__":
+    
+    print(Factory.initRepo())
