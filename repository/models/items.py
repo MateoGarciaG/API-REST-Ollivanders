cbc6043af30d9eeb64e54a7a9b561a1c77565224
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-06 19:48:31 +0200

    feat: :sparkles: Added a new method called: to_json() that help us to convert the Item to JSON format with a Python Dictionary

--- repository/models/items.py
+++ repository/models/items.py
@@ -26,13 +26,23 @@ class Items(db.Model):
             sell_in (int): Sell_in of the Item
             quality (int): Quality of the item
         """
-        
+        # The reason of because ID is not put here is because in MySQL the primary Key is done automatically
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
     
     def __repr__(self):
         return "id: {0} | name: {1} | sell_in: {2} | quality: {3}".format(self.id, self.name, self.sell_in, self.quality)
+    
+    def to_json(self):
+        """Return the Item in a Dictionary with the reason of use this Item more useful when it managed with JSON.
+
+        Returns:
+            (dict): Returns a representation of the Item of JSON in format Python Dictionary
+        """
+        
+        return {"name":self.name, "sell_in":self.sell_in, "quality":self.quality}
+        
     
     
 # if __name__ == "__main__":
