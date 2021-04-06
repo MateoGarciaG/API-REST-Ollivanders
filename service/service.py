892ce4e5155f228a64556f5626bc48db88324945
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-06 23:30:04 +0200

    feat: :sparkles: Added @marshal_with to some methods that returns a result

--- service/service.py
+++ service/service.py
@@ -7,7 +7,11 @@ from flask import jsonify
 # Importamos la Database Class con los métodos
 from repository.db import *
 class Service():
+    """Service: This class consist of a serie of methods that interact with Database/repository Module, where through these methods we can get or manipulate the data of database to send to the client with the Response
+
+    """
     
+    # Estructura de campos que serán usados en el marshal_with()
     resource_fields = {
             'name': fields.String,
             'sell_in': fields.Integer,
@@ -22,15 +26,18 @@ class Service():
     
     
     @staticmethod
+    @marshal_with(resource_fields)
     def filter_by_name(name):
         return Service.check_items(Database.filter_by_name(name))
     
     @staticmethod
+    @marshal_with(resource_fields)
     def filter_by_sell_in(sell_in):
         return Service.check_items(Database.filter_by_sell_in(sell_in))
     
 
     @staticmethod
+    @marshal_with(resource_fields)
     def filter_by_quality(quality):
         return Service.check_items(Database.filter_by_quality(quality))
     
@@ -41,7 +48,6 @@ class Service():
         return Database.get_items()
     
     @staticmethod
-    @marshal_with(resource_fields)
     def post_items(args_content):
         
         Database.post_item(args_content)
@@ -49,7 +55,6 @@ class Service():
         return jsonify({"state": "The Item was added."})
     
     @staticmethod
-    @marshal_with(resource_fields)
     def delete_items(args_content):
         
         Database.delete_item(args_content)
