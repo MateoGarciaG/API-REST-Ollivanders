d2bd34e2ed5386f56d20e77d31f330ab872b51c1
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-07 13:40:31 +0200

    feat: :sparkles: Added into create_app() method, init_app() method to init Flask App with Db Connection and added all resources into Api Object

--- controller/factory.py
+++ controller/factory.py
@@ -2,27 +2,36 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 # Import Resources
+from resources.wellcome import Wellcome
 from resources.inventario import Inventario
 from resources.updateQuality import UpdateQuality
 from resources.items import Items
 from resources.quality import Quality
 from resources.sellin import SellIn
 
+# Import from Repository the db_connection.py
+from repository import db_connection
+
 def create_app():
 
     app = Flask(__name__)
+    
+    # Init the Flask APP
+    db_connection.init_app()
 
     #API REST to be able to test
     api = Api(app, catch_all_404s=True)
 
-    class HelloOllivanders(Resource):
-        def get(self):
-            return {"hello": "Ollivander"}
-
-    api.add_resource(HelloOllivanders, '/')
+    # Add Resources
+    api.add_resource(Wellcome, '/')
+    api.add_resource(Inventory, '/inventory')
+    api.add_resource(Items, '/items/name/<string:item_name>', '/items')
+    api.add_resource(Sellin, '/items/sellin/<int:item_sell_in>')
+    api.add_resource(Quality, '/items/quality/<int:item_quality>')
+    api.add_resource(UpdateQuality, '/update_quality')
 
     return app
 
 if __name__=='__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
