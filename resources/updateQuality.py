7945209f7fe89036852a372e99fb0fcc6a40d62b
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-07 13:04:02 +0200

    docs: :memo: Added Documentation of get() method of UpdateQuality Resource

--- resources/updateQuality.py
+++ resources/updateQuality.py
@@ -5,5 +5,9 @@ from service.service import Service
 class UpdateQuality(Resource):
     
     def get(self):
-        
+        """Gets a list of all items with their quality updated
+
+        Returns:
+            list: Returns a List with items, each item is a dictionary
+        """
         return Service.update_quality()
