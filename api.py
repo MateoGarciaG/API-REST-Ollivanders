a42c4ed2a06c612d76cdb83aa52eaac460bb0db7
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-07 13:46:22 +0200

    feat: :sparkles: Added content from factory.py controller into api.py to tring to execute api.py

--- api.py
+++ api.py
@@ -1,23 +1,34 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-# Repository Module
-from repository.db_connection import init_app
+# Import Resources
+from resources.wellcome import Wellcome
+from resources.inventario import Inventario
+from resources.updateQuality import UpdateQuality
+from resources.items import Items
+from resources.quality import Quality
+from resources.sellin import SellIn
 
-#* App FLASK
+# Import from Repository the db_connection.py
+from repository import db_connection
+
+
 app = Flask(__name__)
 
-# Init APP
-init_app(app)
+# Init the Flask APP
+db_connection.init_app()
 
-# API RESTful
-api = Api(app)
+#API REST to be able to test
+api = Api(app, catch_all_404s=True)
 
-class HelloOllivanders(Resource):
-    def get(self):
-        return {'hello': 'Ollivander'}
+# Add Resources
+api.add_resource(Wellcome, '/')
+api.add_resource(Inventory, '/inventory')
+api.add_resource(Items, '/items/name/<string:item_name>', '/items')
+api.add_resource(Sellin, '/items/sellin/<int:item_sell_in>')
+api.add_resource(Quality, '/items/quality/<int:item_quality>')
+api.add_resource(UpdateQuality, '/update_quality')
 
-api.add_resource(HelloOllivanders, '/')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
