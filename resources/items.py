2f4ba33178a94957b39bf14b303a4234d4323c3b
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-07 12:18:29 +0200

    docs: :memo: Added documentation of get() method os Items Resource

--- resources/items.py
+++ resources/items.py
@@ -6,5 +6,13 @@ from repository.models.items import Items
 class Items(Resource):
     
     def get(self, item_name):
+        """Get an item by its name
+
+        Args:
+            item_name (string): name of the item
+
+        Returns:
+            list: Returns a list with all item whose satified the criteria of hve the same name of the parameter
+        """
         
         return Service.get_items(item_name), 200
