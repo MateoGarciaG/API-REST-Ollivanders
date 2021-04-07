c0c122aa32f71c918f3bce071770045a01d82ad1
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-07 13:28:50 +0200

    docs: :memo: Added documentation of get() method of Wellcome Resource

--- resources/wellcome.py
+++ resources/wellcome.py
@@ -5,4 +5,9 @@ from repository.models import Item
 class Wellcome(Resource):
 
     def get(self):
+        """Get a message of Welcome Ollivanders
+
+        Returns:
+            dict: Returns a dictionary with the wellcome message
+        """
         return {'Welcome!': 'Ollivanders'}
