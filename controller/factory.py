9edc010a4250456650d1fee0a1bfb333b52cb600
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-09 15:41:21 +0200

    perf: :zap: Adapting factory with create_app() to the TESTING Environment, this help us to say to get_db() that use the Tesring Environment

--- controller/factory.py
+++ controller/factory.py
@@ -26,10 +26,15 @@ def create_app():
     # else:
     #     #Configuration APP Flask
     #     app.config.from_object("config.DevelopmentConfig")
+    
+    # Debido a que este Factory -> create_app() de Flask es para poder testear los resources y ver si funcionan, lo mejor es establecer el "Environment" en TEST y activar el TESTING
+    app.config["ENV"] = "test"
+    app.config["TESTING"] = True
 
     # Init the Flask APP
     # db_connection_mock.init_app(app)
-    # db_connection.init_app(app)
+    # Debido a que el APP Flask ya tiene el "Environment" 
+    db_connection.init_app(app)
 
     #API REST to be able to test
     api = Api(app, catch_all_404s=True)
@@ -48,4 +53,5 @@ def create_app():
 
 if __name__=='__main__':
     app = create_app()
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
     app.run(host="0.0.0.0", port=5000, debug=True)
